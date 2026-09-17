219fb687058208c8230063148051f2ff40c46d83
Anastasia <nastyabarsuuko@gmail.com>
2026-09-17 22:38:47 +0700

    docs: добавлены комментарии к функциям по замечанию ревью

--- barsukova.cpp
+++ barsukova.cpp
@@ -2,11 +2,12 @@
 
 #define COEFF 1.609344	// коэффициент для перевода 
 
+// Перевод из мили/ч в км/ч
 double mphToKmh(double v) {
 	return v * COEFF;
 }
 
-
+// Перевод из км/ч в мили/ч
 double kmhToMph(double v) {
 	return v / COEFF;
 }
